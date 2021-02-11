8a123fc5ad63e85410a16d47e450f2ef2c535232
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2021-02-11 11:13:12 +0000

    Finished almost everything

--- TP1/EX_10.c
+++ TP1/EX_10.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main(){
+
     for(int i = 0; i < 100000; i++){
         printf("Hello, World!");
     }
+
+    system("bash -c \" time ./EX_10.c \"");
+    
     return 0;
 }
