84108018f4977973048056694ece03314e401296
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2021-02-17 09:50:16 +0000

    Exercise 4B is now working well

--- JAS-Extra/EX_4B.c
+++ JAS-Extra/EX_4B.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-/*This program just salutes the person with the name given by the exercise */
+/*This program just salutes the person with the name given by the shell and in the number of times specified */
 
 int main(int argc, char* argv[]){
-    if(argc == 2){
-        printf("Hello %s !\n", argv[1]);
-        return 0;
-    }
-    else if(argc > 2){
-        printf("Hello");
+    if(argc >= 2){
+        int rep = 0;
         for(int i = 1; argv[i] != NULL; i++){
-            printf(" %s", argv[i]);
+            if(atoi(argv[i])){
+                rep = atoi(argv[i]);
+            }
         }
-        printf("!\n");
+        for(int i = 0; i < rep; i++){
+            printf("Hello");
+
+            for(int i = 1; argv[i + 1] != NULL; i++){
+                printf(" %s", argv[i]);
+            }
+
+            printf("!\n");
+        }
         return 0;
     }
 }
